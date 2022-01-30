79f221ffe8e1769dffbc616cdaad1d48e033f04c
vpoupet <victor.poupet@gmail.com>
2022-01-30 13:43:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# TP3 - Calculs distribués
+
+Ce TP est une illustration des applications qui utilisent des calculs distribués pour les accélérer.
+
+## Multiplication de matrices
+
+**Rappels :** Le produit de deux matrices ne peut se définir que si le nombre de colonnes de la première matrice est le même que le nombre de lignes de la deuxième matrice, c’est-à-dire lorsqu’elles sont de tailles compatibles.
+
+Si _A_ = (_a<sub>ij</sub>_) est une matrice de dimensions (_m_, _n_) et _B_ = (_b<sub>ij</sub>_) est une matrice de dimensions (_n_, _p_), alors leur produit, noté _AB_ = (_c<sub>ij</sub>_) est une matrice de dimensions (_m_, _p_) donnée par :
+<div style="text-align: center"><img src="https://latex.codecogs.com/svg.image?\forall i,j, \quad c_{ij} = \sum_{k=1}^{n}a_{ik}b_{kj}" title="produit matrices" /></div>
+
+1. Regardez la classe `Matrix` fournie, qui permet de manipuler des matrices d'entiers.
+
+2. Complétez la méthode `times(Matrix B)` de la classe `Matrix`. Testez votre méthode sur des exemples.
+
+Le produit de deux matrices peut être calculé _par blocs_ en utilisant la formule suivante[^1] :
+
+Si on considère les matrices
+
+<div style="text-align: center"><img src="https://latex.codecogs.com/svg.image?M = \left(\begin{array}{cc}A&B\\C&D\end{array}\right)\qquad N = \left(\begin{array}{cc}A'&B'\\C'&D'\end{array}\right)" title="produit matrices" /></div>
+
+où _A_, _A'_, _B_, _B'_, _C_, _C'_, _D_ et _D'_ sont des (sous-)matrices telles que
+- le nombre de colonnes de _A_ et _C_ est égal au nombre de lignes de _A'_ et _B'_
+- le nombre de colonnes de _B_ et _D_ est égal au nombre de lignes de _C'_ et _D'_
+
+On a alors le produit
+<div style="text-align: center"><img src="https://latex.codecogs.com/svg.image?M.N = \left(\begin{array}{cc}AA'+BC'&AB'+BD'\\CA'+DC'&CB'+DD'\end{array}\right)" title="produit matrices" /></div>
+
+Cette décomposition permet de calculer le produit de façon distribuée.
+
+2. Proposez une méthode qui calcule le produit selon une décomposition en blocs des opérandes, en parallélisant les calculs indépendants.
+
+    **Indication :** Vous pouvez définir une tâche qui calcule le produit de deux sous-matrices (par exemple en fournissant les matrices initiales et les sous-intervalles à considérer), et utiliser une tâche principale qui appelle 8 tâches pour faire les sous-produits et construit la matrice finale lorsque les tâches sont complétées.
+
+3. Si on définit la complexité d'une multiplication de matrices comme étant le nombre d'opérations arithmétiques élémentaires (additions et multiplications de nombres) exécutées, quel est la complexité de la multiplication de deux matrices _n_×_n_ par l'algorithme classique (sans parallélisme) ? 
+
+4. Dans le cas distribué, si on suppose que les tâches sont exécutées en parallèle, la complexité est le nombre d'opérations élémentaires du plus long "chemin d'exécution" (c'est-à-dire la complexité de la sous-tâche la plus longue, plus la complexité de la tâche principale qui regroupe les résultats). Quelle est alors la complexité de la multiplication de deux matrices _n_×_n_ par l'algorithme distribué ?
+
+[^1]: Réf: [Produit matriciel](https://fr.wikipedia.org/wiki/Produit_matriciel) sur Wikipédia
---
+++ src/Matrix.java
@@ -0,0 +1,135 @@
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Random;
+import java.util.StringJoiner;
+
+/******************************************************************************
+ *  Représentation des matrices d'entiers
+ *  Source : https://introcs.cs.princeton.edu/java/95linear/Matrix.java
+ ******************************************************************************/
+
+final public class Matrix {
+    private final int m;             // nombre de lignes
+    private final int n;             // nombre de colonnes
+    private final int[][] data;   // tableau de n x m éléments
+
+    /**
+     * Constructeur d'une matrice m x n de zéros
+     */
+    public Matrix(int m, int n) {
+        this.m = m;
+        this.n = n;
+        data = new int[m][n];
+    }
+
+    /**
+     * Constructeur d'une matrice à partir d'un tableau de valeurs (le tableau est copié)
+     */
+    public Matrix(int[][] data) {
+        m = data.length;
+        n = data[0].length;
+        this.data = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                this.data[i][j] = data[i][j];
+            }
+        }
+    }
+
+    /**
+     * Constructeur par copie
+     */
+    public Matrix(Matrix A) {
+        this(A.data);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Matrix matrix = (Matrix) o;
+        return m == matrix.m && n == matrix.n && Arrays.deepEquals(data, matrix.data);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(m, n);
+        result = 31 * result + Arrays.deepHashCode(data);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner joiner = new StringJoiner("\n");
+        for (int i = 0; i < m; i++) {
+            StringJoiner rowJoiner = new StringJoiner(" ");
+            for (int j = 0; j < n; j++) {
+                rowJoiner.add(String.format("%3d", data[i][j]));
+            }
+            joiner.add(rowJoiner.toString());
+        }
+        return joiner.toString();
+    }
+
+    /**
+     * Renvoie une matrice m x n contenant des valeurs aléatoires entre -10 et 10 (inclus)
+     */
+    public static Matrix random(int m, int n) {
+        Random rd = new Random();
+        Matrix A = new Matrix(m, n);
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                A.data[i][j] = (rd.nextInt(-10, 11));
+        return A;
+    }
+
+    /**
+     * Renvoie la matrice identité de taille n
+     */
+    public static Matrix identity(int n) {
+        Matrix I = new Matrix(n, n);
+        for (int i = 0; i < n; i++)
+            I.data[i][i] = 1;
+        return I;
+    }
+
+    /**
+     * Renvoie la somme matricielle (this + B)
+     * Prérequis : A et B sont de mêmes dimensions
+     */
+    public Matrix plus(Matrix B) {
+        Matrix C = new Matrix(m, n);
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                C.data[i][j] = this.data[i][j] + B.data[i][j];
+        return C;
+    }
+
+
+    /**
+     * Renvoie la différence matricielle (this - B)
+     * Prérequis : A et B sont de mêmes dimensions
+     */
+    public Matrix minus(Matrix B) {
+        Matrix C = new Matrix(m, n);
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                C.data[i][j] = this.data[i][j] - B.data[i][j];
+        return C;
+    }
+
+    /**
+     * Renvoie le produit matriciel (this * B)
+     * Prérequis : A et B sont de dimensions compatibles
+     */
+    public Matrix times(Matrix B) {
+        // À compléter
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Matrix A = Matrix.random(8, 8);
+        System.out.println(A);
+        System.out.println();
+    }
+}
